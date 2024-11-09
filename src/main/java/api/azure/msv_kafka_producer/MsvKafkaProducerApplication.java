f53eaa541352cedd40bced2c23b0933a301fb21d
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-11-09 15:52:58 -0500

    init msv kafka producer

---
+++ src/main/java/api/azure/msv_kafka_producer/MsvKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package api.azure.msv_kafka_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/azure/msv_kafka_producer/MsvKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package api.azure.msv_kafka_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
